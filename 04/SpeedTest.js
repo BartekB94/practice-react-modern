ef60a4009c01313cdd3ff7d0a0e7c99ff36dd40d
BartekB94 <wurir1994@gmail.com>
2024-08-06 14:07:03 +0200

    done 04

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,19 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useRandomItem from './hook';
 
-const SpeedTest = () => {
+function SpeedTest() {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [inputText, setInputText] = useState('');
+    const [time, setTime] = useState(0);
+    const [totalChars, setTotalChars] = useState(0);
+    const inputRef = useRef(null);
+    const intervalRef = useRef(null);
 
     useEffect(() => {
         regenerateWord();
     }, []);
 
+    const handleFocus = () => {
+        if (!intervalRef.current) {
+            intervalRef.current = setInterval(() => {
+                setTime((prevTime) => prevTime + 1);
+            }, 1000);
+        }
+    };
+
+    const handleBlue = () => {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+    };
+
+    useEffect(() => {
+        if (inputText === word) {
+            setTotalChars((prevTotal) => prevTotal + word.length);
+            setInputText('');
+            regenerateWord();
+        }
+    }, [inputText, word]);
+
+    useEffect(() => () => clearInterval(intervalRef.current), []);
+
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <input
+                ref={inputRef}
+                value={inputText}
+                onFocus={handleFocus}
+                onBlur={handleBlue}
+                onChange={(e) => setInputText(e.target.value)}
+            />
+            <p>Time: {time}</p>
+            <p>Total chars typed: {totalChars}</p>
         </div>
     );
-};
+}
 
 export default SpeedTest;
